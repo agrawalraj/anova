e02c392c607b4a04c642fbbafaf91ac49ef1af7a
Raj <raj@Rajs-MacBook-Pro-3.local>
2023-01-27 04:27:12 -0500

    added basis check

--- fova/basis/maps.py
+++ fova/basis/maps.py
@@ -1,4 +1,7 @@
 
+import jax.numpy as jnp
+from jax import vmap
+
 
 class FiniteDimensionalBasis(object):
 	def __init__(self, X_train, basis_config):
@@ -28,10 +31,33 @@ class TreeBasis(object):
 		self._mean = X_train.mean(axis=0)
 		self._sd = X_train.std(axis=0)
 
-	def transform(self, X):
-		N, p = X.shape
-		X_feat = (X - self._mean) / self._sd
-		return X_feat.reshape((N, p, 1))
+	def transform(self, X, normalize=True):
+		if normalize:
+			N, p = X.shape
+			X_feat = (X - self._mean) / self._sd
+			return X_feat.reshape((N, p, 1))
+		return X.copy().reshape((N, p, 1))
+
+
+class RepeatedFiniteBasis(object):
+	def __init__(self, X_train, basis1d):
+		self.basis1d = basis1d
+		self.basis_mapped = vmap(basis1d)
+		X_feat = jnp.transpose(self.basis_mapped(X_train), axes=(0, 2, 1))
+		self._basis_mean = X_feat.mean(axis=0)
+		self._basis_sd = X_feat.std(axis=0)
+
+	def transform(self, X, normalize=True):
+		X_feat = jnp.transpose(self.basis_mapped(X), axes=(0, 2, 1))
+		if normalize:
+			return (X_feat - self._basis_mean) / self._basis_sd
+		else:
+			return X_feat
+
+
+class BasisComposer(object):
+	# LinearBasis (zero mean input) ---> repeated finite basis 
+	pass
 
 
 # @jax.jit
--- tests/kernels/test_skim.py
+++ tests/kernels/test_skim.py
@@ -2,18 +2,37 @@
 import numpy as np
 import jax.numpy as jnp
 from jax import random
-from fova.kernels.skim import *
 from sklearn.preprocessing import PolynomialFeatures
 
+from fova.kernels.skim import *
+from fova.basis.maps import RepeatedFiniteBasis
 
-def make_feature_map(X, Q):
+
+def make_feature_map(X, Q, include_bias=True):
 	# Generate all interaction of order up to Q
-	feat_map = PolynomialFeatures(degree=Q, interaction_only=True)
+	feat_map = PolynomialFeatures(degree=Q, interaction_only=True, 
+									include_bias=include_bias)
 	return jnp.array(feat_map.fit_transform(np.array(X)))
 
 
 def relative_error(K_est, K_true):
 	return jnp.abs(K_est - K_true).sum() / jnp.abs(K_true).sum()
+
+
+def two_dimensional_feat_map_helper(X):
+	assert X.shape[1] == 2
+	N = X.shape[0]
+	ones = jnp.ones((N, 1))
+	X1 = X[:, 0].reshape((N, 1))
+	X2 = X[:, 1].reshape((N, 1))
+	X1_sq = (X1 * X1).reshape((N, 1))
+	X2_sq = (X2 * X2).reshape((N, 1))
+	X1_X2 = (X1 * X2).reshape((N, 1))
+	X1_sq_X2_sq = (X1_sq * X2_sq).reshape((N, 1))
+	X1_X2_sq = (X1 * X2_sq).reshape((N, 1))
+	X1_sq_X2 = (X1_sq * X2).reshape((N, 1))
+	return jnp.concatenate([ones, X1, X2, X1_sq, X2_sq, X1_X2, 
+							X1_sq_X2_sq, X1_X2_sq, X1_sq_X2], axis=1)
 
 
 def test_skim_matches_weight_space_view_linear():
@@ -38,6 +57,35 @@ def test_skim_matches_weight_space_view_linear():
 
 		# Kernel matrix from explicit map
 		X_explicit = make_feature_map(X, Q)
+		K_explicit = X_explicit.dot(X_explicit.T)
+
+		assert relative_error(K_skim, K_explicit) < 1e-5
+
+
+def test_skim_matches_weight_space_view_basis():
+	# Create random data
+	N = 100
+	p = 2
+	key = random.PRNGKey(0)
+	X = random.normal(key, shape=(N, p))
+	basis1d = lambda x: jnp.array([x, x ** 2])
+	basis = RepeatedFiniteBasis(X, basis1d)
+
+	for Q in [2]:
+		# SKIM kernel
+		kernel_params = dict()
+		c = 0.
+		kernel_params['U_tilde'] = 1e6 * jnp.ones(p)
+
+		# Choice of u_tilde and c should make kappa basically all ones
+		assert jnp.abs(get_kappa(kernel_params['U_tilde'], c) - jnp.ones(p)).sum() < 1e-5
+
+		kernel_params['eta'] = jnp.ones(Q+1)
+		K_skim = skim_kernel_matrix(basis.transform(X, False), 
+									basis.transform(X, False), c, kernel_params)
+
+		# Kernel matrix from explicit map
+		X_explicit = two_dimensional_feat_map_helper(X)
 		K_explicit = X_explicit.dot(X_explicit.T)
 
 		assert relative_error(K_skim, K_explicit) < 1e-5
