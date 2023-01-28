2161202283a891f39b561f21085d12e2fdb3e55a
Raj <raj@Rajs-MacBook-Pro-3.local>
2023-01-28 02:50:51 -0500

    finished writing test for testing getting effects via tensor product formula

--- fova/decomposers/tensor_product.py
+++ fova/decomposers/tensor_product.py
@@ -27,6 +27,16 @@ class TensorProductKernelANOVA(Decomposer):
 
 
 class LinearANOVA(TensorProductKernelANOVA):
+    def get_effect(self, X_feat, V):
+        if len(V) == 0: # Constant / intercept term
+            eta = self.kernel_params['eta']
+            alpha = self.alpha
+            return eta[0] * alpha.sum() * jnp.ones(X_feat.shape[0])
+        else:
+            beta = self.get_coef(V)
+            N, p, __ = X_feat.shape
+            return beta * jnp.product(X_feat.reshape((N, p))[:, jnp.array(V)], axis=1)
+
     def get_coef(self, V):
         if len(set(V) - set(self.selected_covs)) > 0:
             return 0.
@@ -34,11 +44,11 @@ class LinearANOVA(TensorProductKernelANOVA):
         
         X_probe = jnp.zeros((1, self.p, 1))
         if len(V) == 0:
-            return self.get_effect(X_probe, V).item()
+            return super().get_effect(X_probe, V).item()
 
         # f_V(x_v) = \theta_V \prod_{i \in V} x_i
         # Set \prod_{i \in V} x_i = 1 by setting x_i = 1
         # Then, f_V(x_v at probe) = theta_V
         X_probe = X_probe.at[:, jnp.array(V)].set(1.)
-        f_V = self.get_effect(X_probe, V)
+        f_V = super().get_effect(X_probe, V)
         return f_V.item()
--- tests/decomposers/test_tensor_product.py
+++ tests/decomposers/test_tensor_product.py
@@ -1,6 +1,100 @@
 
-from fova.decomposers.tensor_product import LinearANOVA
+import numpy as np
+import jax.numpy as jnp
+from jax import random
+from sklearn.preprocessing import PolynomialFeatures
+
+from fova.kernels.skim import *
+from fova.basis.maps import LinearBasis
+from fova.decomposers.tensor_product import TensorProductKernelANOVA, LinearANOVA
+from fova.inference.fit import GaussianSKIMFA
+from fova.misc.scheduler import truncScheduler
+from fova.misc.logger import GausLogger
+from sklearn.linear_model import Ridge
 
 
-def test_linear_anova():
-	anova_fn = LinearANOVA()
+def make_feature_map(X, Q, include_bias=True):
+	# Generate all interaction of order up to Q
+	feat_map = PolynomialFeatures(degree=Q, interaction_only=True, 
+									include_bias=include_bias)
+
+	X_feat = feat_map.fit_transform(np.array(X))
+	return feat_map.get_feature_names_out(), X_feat
+
+
+def test_anova():
+	key = random.PRNGKey(0)
+	N = 200
+	p = 5
+	X = random.normal(key, shape=(N, p))
+	Y = X[:, 0] + X[:, 1] + X[:, 2] * X[:, 3]
+
+	X_train = X[:100, :]
+	Y_train = Y[:100]
+
+	X_valid = X[100:, :]
+	Y_valid = Y[100:]
+
+	kernel_params = dict()
+	Q = 2
+	kernel_params['U_tilde'] = 1e6 * jnp.ones(p)
+	kernel_params['eta'] = jnp.ones(Q+1)
+
+	hyperparams = dict()
+	hyperparams['sigma_sq'] = .5 * jnp.var(Y)
+	hyperparams['c'] = 0.
+
+	opt_params = dict()
+	opt_params['cg'] = True
+	opt_params['cg_tol'] = .001
+	opt_params['M'] = 20
+	opt_params['gamma'] = 0 # don't update params
+	opt_params['T'] = 1
+
+	featprocessor = LinearBasis(X_train)
+	scheduler = truncScheduler()
+	logger = GausLogger(100)
+
+	X_normed = featprocessor.transform(X)
+	X_normed = X_normed.reshape((N, p))
+
+	opt_params['scheduler'] = scheduler
+
+	skim = GaussianSKIMFA(X_train, Y_train, X_valid, Y_valid, featprocessor)
+
+	skim.fit(key, hyperparams, kernel_params, opt_params, 
+	            logger=GausLogger())
+
+	decompose = TensorProductKernelANOVA(skim)
+	anova = decompose.get_decomposition(X_train)
+
+	linear_decompose = LinearANOVA(skim)
+	lanova = linear_decompose.get_decomposition(X_train)
+
+	print('Testing SKIM formula vs. special linear SKIM formula')
+	for V, effect in anova.items():
+		leffect = lanova[V]
+		assert jnp.abs((effect - leffect)).sum() < 1e-4
+
+	print('\n')
+
+	# Compare difference with explicit regression
+	print('Testing linear SKIM formula vs. explicit regression')
+	feat_names, X_explicit = make_feature_map(np.array(X_normed), Q)
+	ridge = Ridge(alpha=hyperparams['sigma_sq'].item(), fit_intercept=False)
+	ridge.fit(X_explicit[:100, :], Y[:100])
+
+	for V, __ in lanova.items():
+		beta = linear_decompose.get_coef(V)
+		if len(V) == 0:
+			effect_ix = np.where(feat_names == '1')[0][0]
+		if len(V) == 1:
+			cov_ix = V[0]
+			effect_ix = np.where(feat_names == f'x{cov_ix}')[0][0]
+		if len(V) == 2:
+			cov_ix1, cov_ix2 = sorted(V)
+			effect_ix = np.where(feat_names == f'x{cov_ix1} x{cov_ix2}')[0][0]
+
+		print(V, ridge.coef_[effect_ix], beta)
+		assert jnp.abs((ridge.coef_[effect_ix] - beta)).sum() < 1e-2
+
