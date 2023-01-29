7c65433b5617767ae848370d0d4f0b4640c4b9b1
Raj <raj@Rajs-MacBook-Pro-3.local>
2023-01-28 21:42:15 -0500

    adding missing theme package

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'python_docs_theme'
+    'pydata_sphinx_theme'
 ]
 
 intersphinx_mapping = {
