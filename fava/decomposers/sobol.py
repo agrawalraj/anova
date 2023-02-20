5d375b517103b404b3b66e9ae68f8c97eb0421d4
Raj <raj@Rajs-MacBook-Pro-3.local>
2023-02-20 02:43:45 -0500

    uncomitted changes

--- fava/decomposers/sobol.py
+++ fava/decomposers/sobol.py
@@ -1,21 +1,27 @@
 
+import jax.numpy as jnp
+from jax import grad, jit, vmap
+from jax import random
+import jax
 
+from fava.basis.maps import Identity
+from fava.decomposers.decomposer import all_subsets
 
 
 class Sobol(Decomposer):
-	def __init__(self, X_train, model):
+	def __init__(self, X_dist, model):
 		self.featprocessor = Identity()
-		self.X_train_shuffled = shuffle(X_train.copy())
+		self.X_dist = shuffle(X_dist.copy())
 		self.model = model
-		self.intercept = mode.predict(X_train).mean() # TODO: replace with predict_proba if needed
+		self.intercept = model.predict(X_dist).mean() # TODO: replace with predict_proba if needed
 
 	def __get_effect_uncorrected(self, X, V):
 		if len(V) == 0:
 			return self.intercept * jnp.ones((X.shape[0],))
 		else:
 			preds = []
-			for xv in X[: V]:
-				X_probe = self.X_train_shuffled.copy()
+			for xv in X[:, V]:
+				X_probe = self.X_dist.copy()
 				X_probe[:, V] = xv
 				preds.append(model.predict(X_probe).mean()) # TODO: replace with predict_proba if needed
 			return jnp.array(preds)
@@ -33,3 +39,13 @@ class Sobol(Decomposer):
 		return effect[V]
 
 
+if __name__ == "__main__":
+	class ExampleModel:
+		def predict(self, X):
+			return X[:, 0] + X[:, 0] + X[:, 0]*X[:, 1]
+
+	model = ExampleModel()
+	N = 100
+	p = 2
+	key = random.PRNGKey(0)
+	X = random.normal(key, shape=(N, p))
